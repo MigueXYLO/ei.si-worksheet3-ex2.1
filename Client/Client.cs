f628d15c7216f02b3b8100a42e55fa9abc67a731
migueXYLO <2221462@my.ipleiria.pt>
2024-03-15 12:35:26 +0000

    Add project files.

---
+++ Client/Client.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EI.SI;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    /// <summary>
+    /// Client
+    /// </summary>
+    class Client
+    {
+        public static string SEPARATOR = "...";
+
+        /// <summary>
+        /// IMPORTANTE: a cada RECEÇÃO deve seguir-se, obrigatóriamente, um ENVIO de dados
+        /// IMPORTANT: each network .Read() must be fallowed by a network .Write()
+        /// </summary>
+        static void Main(string[] args)
+        {
+            byte[] msg;
+            IPEndPoint serverEndPoint;
+            TcpClient client = null;
+            NetworkStream netStream = null;
+            ProtocolSI protocol = null;
+
+            try
+            {
+                Console.WriteLine("CLIENT");
+
+                #region Definitions
+                // Client/Server Protocol to SI
+                protocol = new ProtocolSI();
+
+                // Defenitions for TcpClient: IP:port (127.0.0.1:9999)
+                serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+                #endregion
+
+                Console.WriteLine(SEPARATOR);
+
+                #region TCP Connection
+                // Connects to Server ...
+                Console.Write("Connecting to server... ");
+                client = new TcpClient();
+                client.Connect(serverEndPoint);
+                netStream = client.GetStream();
+                Console.WriteLine("ok.");
+                #endregion
+
+                Console.WriteLine(SEPARATOR);
+
+                #region Exchange Data (Unsecure channel)
+                // Send data...
+                string clearData = "hello world!!!";
+                Console.Write("Sending data... ");
+                msg = protocol.Make(ProtocolSICmdType.DATA, clearData);
+                netStream.Write(msg, 0, msg.Length);
+                Console.WriteLine("ok.");
+                Console.WriteLine("   Sent: {0} = {1}", clearData, ProtocolSI.ToHexString(Encoding.UTF8.GetBytes(clearData)));
+
+                // Receive answer from server
+                Console.Write("waiting for ACK... ");
+                netStream.Read(protocol.Buffer, 0, protocol.Buffer.Length);
+                Console.WriteLine("ok.");
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(SEPARATOR);
+                Console.WriteLine("Exception: {0}", ex.ToString());
+            }
+            finally
+            {
+                // Close connections
+                if (netStream != null)
+                    netStream.Dispose();
+                if (client != null)
+                    client.Close();
+                Console.WriteLine(SEPARATOR);
+                Console.WriteLine("Connection with server was closed.");
+            }
+
+            Console.WriteLine(SEPARATOR);
+            Console.Write("End: Press a key...");
+            Console.ReadKey();
+        }
+    }
+}
